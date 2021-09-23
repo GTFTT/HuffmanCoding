00d8ca43a3d356ff1e8e573a1f401d47456570d7
GT <FTowerT@outlook.com>
2021-09-23 21:01:07 +0300

    Code refactoring and cleaning

--- src/pages/HuffmanCoding/index.js
+++ src/pages/HuffmanCoding/index.js
@@ -5,21 +5,6 @@ import React, { useState, useEffect } from 'react';
 
 import GraphGenerator from './components/GraphGenerator';
 
-const getStatistic = (text) => {
-    if(!text || text === "") return {};
-    const mapObj = {};
-
-    for (var i = 0; i < text.length; i++) {
-        const char = text.charAt(i);
-
-        if(mapObj[char]) {
-            mapObj[char].count++;
-        } else {
-            mapObj[char] = { count: 1 };
-        }
-    }
-    return mapObj
-}
 
 //Sort in descending order based on count
 const comparatorObj = (a, b) => {
@@ -34,18 +19,41 @@ const comparator = (a, b, stats) => {
     return 1;
 }
 
-function getGenerationArray(stats) {
-    const firstLevel = Object.keys(stats).sort((a, b) => comparator(a, b, stats)).map(key => {
-        // return <div>{key} - { stats[key].count }</div>
-        return {count: stats[key].count, character: key}
-    })
+const getInitialHuffmanArray = (text) => {
+    if(!text || text === "") return [];
+    const mapOfFrequency = {};
 
-    const buffer = firstLevel.map(val => val);
-    const maxIter = 1000;
+    for (var i = 0; i < text.length; i++) {
+        const char = text.charAt(i);
+
+        if(mapOfFrequency[char]) {
+            mapOfFrequency[char].count++;
+        } else {
+            mapOfFrequency[char] = { count: 1 };
+        }
+    }
+
+    const initialHuffmanArray = Object.keys(mapOfFrequency)
+        .sort((a, b) => comparator(a, b, mapOfFrequency))
+        .map(key => {
+            return {count: mapOfFrequency[key].count, character: key}
+        });
+
+    return initialHuffmanArray;
+}
+
+function getGenerationArray(initialHuffmanArray) {
+    const buffer = initialHuffmanArray.map(val => val); //Create copy to prevent array from overwriting
+    const maxIter = 100; //Prevent infinity loop
     let iterCount = 0;
 
     while (buffer.length > 1 && iterCount < maxIter) {
         iterCount++;
+
+        //sort in desc order
+        buffer.sort(comparatorObj);
+
+        //Replace last two with a new one
         const last = buffer.pop();
         const prevLast = buffer.pop();
         const newObj = {
@@ -56,31 +64,20 @@ function getGenerationArray(stats) {
 
         //paste a new one
         buffer.push(newObj);
-
-        //sort
-        buffer.sort(comparatorObj);
     }
-
-    console.log(buffer);
 
     return buffer;
 }
 
 export default () => {
     const [text, setText] = useState("");
-    const [stats, setStats] = useState({});
     const [generationArray, setGenerationArray] = useState([]);
 
     useEffect(() => {
-        const statsRes = getStatistic(text);
-        setStats(statsRes);
+        const initHuffmanArr = getInitialHuffmanArray(text);
+        const generationTreeObject = getGenerationArray(initHuffmanArr);
+        setGenerationArray(generationTreeObject);
     }, [text]);
-
-    useEffect(() => {
-        const genArr = getGenerationArray(stats);
-        setGenerationArray(genArr);
-    }, [stats]);
-    
 
     return (
         <div>
@@ -93,10 +90,9 @@ export default () => {
                     const res = newText.replace(/[^A-Za-z0-9]/gi, '');;
                     setText(res);
                 }}
-            ></input>
-            <button>OK</button>
+            />
             <hr />
-            <GraphGenerator generationArray={generationArray} />
+            <GraphGenerator generationArray={ generationArray } />
         </div>
     );
 }
