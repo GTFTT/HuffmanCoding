9920a5cf9c6de6a68c88a8fc89b9f655c110a9d2
GT <FTowerT@outlook.com>
2021-09-24 15:29:36 +0300

    Overall style improved

--- src/components/AboutHuffman/index.js
+++ src/components/AboutHuffman/index.js
@@ -5,19 +5,25 @@ import HuffmanImage from './images/HuffmanImage.png';
 
 function AboutHuffman() {
     return (
-        <div className={Styles.mainCont}>
-            <div>
-                <img className={Styles.huffmanTreeImage} src={ HuffmanImage } alt="" />
+        <div>
+            <div className={Styles.title}>
+                About
             </div>
-            <div className={Styles.text}>
-                Huffman coding is a lossless data compression algorithm.
-                The idea is to assign variable-length codes to input characters,
-                lengths of the assigned codes are based on the frequencies of corresponding characters.
-                The most frequent character gets the smallest code and the least frequent character gets the largest code.
-                <br/>
-                <a href={ "https://en.wikipedia.org/wiki/Huffman_coding" }>On wikipedia.</a>
+            <div className={Styles.mainCont}>
+                <div>
+                    <img className={Styles.huffmanTreeImage} src={ HuffmanImage } alt="" />
+                </div>
+                <div className={Styles.text}>
+                    Huffman coding is a lossless data compression algorithm.
+                    The idea is to assign variable-length codes to input characters,
+                    lengths of the assigned codes are based on the frequencies of corresponding characters.
+                    The most frequent character gets the smallest code and the least frequent character gets the largest code.
+                    <br/>
+                    <a href={ "https://en.wikipedia.org/wiki/Huffman_coding" }>On wikipedia.</a>
+                </div>
             </div>
         </div>
+        
     );
 }
 
