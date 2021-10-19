bd2602c97cd88b9c8137c7cdec3c15acb998ecd8
GT <FTowerT@outlook.com>
2021-10-19 11:21:24 +0300

    Added generation of full binary code for Decoding

--- src/components/HuffmanDecoding/index.js
+++ src/components/HuffmanDecoding/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Styles from './styles.module.css';
 
-const HuffmanDecoding = (props) => {
-  console.log(props);
-  
+const HuffmanDecoding = ({ generatedCodes }) => {
+  const [fullBinCode, setFullBinCode] = useState("");
+
+  useEffect(() => {
+    let newBinCode = "";
+    for(let [ , binCode] of generatedCodes) {
+      newBinCode += binCode;
+    }
+    setFullBinCode(newBinCode);
+  }, [ generatedCodes ]);
+
   return (
     <div className={Styles.HuffmanDecoding}>
-      HuffmanDecoding Component
+      <h2>Decoded value</h2>
+      <h3>{fullBinCode}</h3>
     </div>
   )
 };
